de76e0ee7c78be5c87cd98dbecaefdd7fe7e8f30
Novel Pham <pvthuyet@gmail.com>
2020-11-25 10:25:42 +0700

    update

--- Cpp20Features/Cpp20Features.cpp
+++ Cpp20Features/Cpp20Features.cpp
@@ -17,6 +17,7 @@ import CpprefGenerator;
 import Fibo.Pmr;
 import Fibo.Pmr2;
 import Fibo.MemoryTracker;
+import range_exam;
 
 using namespace std;
 
@@ -85,7 +86,11 @@ int main()
 		//monotonic_unreusable();
 		//synchronized_reusable();
 
-		fibo::pmr2::testPmr2();
+		//fibo::pmr2::testPmr2();
+
+		// ranges **********************
+		fibo::ranges::exam1();
+
 	}
 	catch (std::exception const& ex)
 	{
